abe9964fcdb60d894d538671a99b5b82731d866e
Dean Fitzgerald <classicmurph@gmail.com>
2015-03-16 16:27:02 -0400

    added facebook oauth html, updated extension and requirements

--- carpool_app/extensions.py
+++ carpool_app/extensions.py
@@ -10,3 +10,6 @@ migrate = Migrate()
 # Change this to HerokuConfig if using Heroku.
 from flask.ext.appconfig import AppConfig
 config = AppConfig()
+
+from flask_oauthlib.client import OAuth
+oauth = OAuth()
---
+++ carpool_app/views/users.py
@@ -0,0 +1,23 @@
+from flask import session, Blueprint, url_for, request, redirect, flash, render_template
+
+from ..extensions import oauth
+
+
+
+facebook = oauth.remote_app('facebook',
+    base_url='https://graph.facebook.com/',
+    request_token_url=None,
+    access_token_url='/oauth/access_token',
+    authorize_url='https://www.facebook.com/dialog/oauth',
+    consumer_key="410559155795197",
+    consumer_secret="f64f456e4558a27416c075b7b9446050",
+    request_token_params={'scope': 'email'}
+)
+
+
+users = Blueprint("users", __name__)
+
+
+@users.route("/")
+def home():
+    return render_template("layout.html")
