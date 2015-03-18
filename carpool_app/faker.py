55bba57003210e379525a6d06659664c4b1df282
Dean Fitzgerald <classicmurph@gmail.com>
2015-03-18 11:08:44 -0400

    seed fake users

--- carpool_app/faker.py
+++ carpool_app/faker.py
@@ -1,12 +1,16 @@
 from faker import Faker
-from .models import User
 from random import random
 
 fake = Faker()
 
 def user_generator():
-    for i in range(20):
-        return User(name=fake.name(), email=fake.email(), facebook_id=random.randint(100,10000))
+    user_list = []
+    for i in range(10):
+        user_list.append({"name": fake.name_male(), "email": "test@test.com", "facebook_id": random.randint(100,10000),
+                "gender": "male"})
+    for i in range(10):
+        user_list.append({"name": fake.name_female(), "email": "test@test.com", "facebook_id": random.randint(100,10000),
+                "gender": "female"})
 
-
+    return user_list
 
