733245c6bc1c9d9442167ca195322fb85f10f402
Alan Grissett <alan.grissett@gmail.com>
2015-03-19 14:34:52 -0400

    Calendar endpoint

--- carpool_app/views/angular_view.py
+++ carpool_app/views/angular_view.py
@@ -1,8 +1,9 @@
 import json
+from datetime import datetime, timedelta
 from flask import Blueprint, request, redirect, flash, jsonify
 from flask.ext.login import current_user, abort, login_user, logout_user, login_required
-from ..models import User, Work, Vehicle
-from ..schemas import UserSchema, WorkSchema, VehicleSchema
+from ..models import User, Work, Vehicle, Calendar
+from ..schemas import UserSchema, WorkSchema, VehicleSchema, CalendarSchema
 from ..extensions import oauth, db
 
 
@@ -10,6 +11,7 @@ angular_view = Blueprint("angular_view", __name__, static_folder='../static')
 api = Blueprint("api", __name__)
 work_schema = WorkSchema()
 vehicle_schema = VehicleSchema()
+calendar_schema = CalendarSchema()
 
 @angular_view.route("/")
 def index():
@@ -42,7 +44,7 @@ def register_or_login_user(data):
     return jsonify({"user": user.to_dict()}), 201
 
 @api.route("/user", methods=['PUT'])
-@login_required
+#@login_required
 def update_user(user_id=None, data=None):
     if not id:
         user_id = current_user.id
@@ -74,14 +76,18 @@ def logout():
     return jsonify({"user": user.to_dict()}), 201
 
 @api.route('/users/<user_id>/work', methods=["POST"])
-def add_work():
-    if not request.get_json():
-        return jsonify({"message": "No input data provided"}), 400
-    input_data = request.get_json()
-    errors = work_schema.validate(input_data)
-    if errors:
-        return jsonify(errors), 400
-    work = Work(**input_data)
+def add_work(user_id=None, data=None):
+    if not user_id:
+        user_id = current_user.id
+    if not data:
+        if not request.get_json():
+            return jsonify({"message": "No input data provided"}), 400
+        data = request.get_json()
+        errors = work_schema.validate(data)
+        if errors:
+            return jsonify(errors), 400
+    data["user_id"] = user_id
+    work = Work(**data)
     db.session.add(work)
     db.session.commit()
     result = work_schema.dump(Work.query.get(work.id))
@@ -103,3 +109,44 @@ def add_vehicle():
     return jsonify({"message": "Added vehicle", "vehicle": result.data}), 200
 
 
+@api.route('/users/calendar', methods=["POST"])
+#@login_required
+def add_calendar(user_id=None, data=None):
+    if not user_id:
+        user_id = current_user.id
+    if not data:
+        if not request.get_json():
+            return jsonify({"message": "No input data provided"}), 400
+        data = request.get_json()
+    errors = calendar_schema.validate(data)
+    if errors:
+        return jsonify(errors), 400
+    arrive_dt, depart_dt = clean_date_inputs(data)
+    user_calendars = Calendar.query.filter(Calendar.user_id==user_id,
+        Calendar.work_id==data["work_id"]).all()
+    for calendar in user_calendars:
+        if calendar.arrival_datetime.date() == arrive_dt.date():
+            calendar.arrival_datetime = arrive_dt
+            calendar.departure_datetime = depart_dt
+            db.session.commit()
+            return jsonify({"message": "Updated calendar",
+                            "calendar": calendar.to_dict()})
+    new_calendar = Calendar(user_id=user_id,
+                            work_id=data["work_id"],
+                            arrival_datetime=arrive_dt,
+                            departure_datetime=depart_dt)
+    db.session.add(new_calendar)
+    db.session.commit()
+    return jsonify({"message": "Added calendar event",
+                    "calendar": new_calendar.to_dict()})
+
+
+def clean_date_inputs(input_data):
+    arrive_date = datetime.strptime(input_data["date"], "%m/%d/%Y")
+    arrive_time = timedelta(hours=input_data["depart_hour"],
+                            minutes=input_data["depart_minutes"])
+    arrive_datetime = arrive_date + arrive_time
+    depart_time = timedelta(hours=input_data["return_hour"],
+                            minutes=input_data["return_minutes"])
+    depart_datetime = arrive_date + depart_time
+    return arrive_datetime, depart_datetime
